17add7423f46779cfb058a67cd21959f23c40a73
Raul Cortinas <114097137+Rcurt17@users.noreply.github.com>
2023-04-06 13:01:23 -0400

    World Series Winners

--- Assignment#7 (Lab).py
+++ Assignment#7 (Lab).py
@@ -1,36 +1,46 @@
 print()
 world_series = open('/Users/rcurt17/Desktop/CS175Work/WorldSeriesWinners.txt', 'r')
 
+world_series_winners = []
+for g in world_series:
+    g = g.rstrip('\n')
+    world_series_winners.append(g)
+
 x = True
 while x:
     count = 0
 
     phrase = str(input("Enter the name of a team: "))
+    once = []
+    broad_search = [line for line in world_series_winners if phrase.lower() in line.lower()]
 
-    with open('WorldSeriesWinners.txt', 'r') as file:
+    for g in broad_search:
+        if g not in once:
+            once.append(g)
 
-        for line in file:
-            line = line.rstrip('\n')
-            if phrase.upper() in line.upper():
-                print(line)
-                count += 1
+    for line in once:
+        print()
+        print(line)
+        count += 1
 
     count_specific = 0
 
-    phrase_specific = str(input("Enter the name of the team you would like to focus on: "))
+    phrase_specific = str(input("\nEnter the name of the team you would like to focus on: "))
 
     with open('WorldSeriesWinners.txt', 'r') as file:
 
         for line in file:
+
             line = line.rstrip('\n')
+
             if phrase_specific.upper() in line.upper():
                 count_specific += 1
 
-    print(f"The team '{phrase_specific}' appears {count_specific} times in the file '{'WorldSeries.txt'}'.")
+    print(f"\nThe team '{phrase_specific}' appears {count_specific} times in the file '{'WorldSeries.txt'}'.")
 
     y = True
     while y:
-        ask = input("Would you like to continue? (y or n) ")
+        ask = input("\nWould you like to continue? (y or n) ")
         if ask.lower() == 'y' or ask.lower() == 'yes':
             x = True
             y = False
